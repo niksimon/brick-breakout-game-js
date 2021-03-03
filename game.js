97db92cc6e60dac5a884602e0bcdc187bb6123b9
simon <nikolasimonovic94@gmail.com>
2021-03-03 21:41:43 +0100

    Rework on main branch, old version is on old branch

--- game.js
+++ game.js
@@ -1,276 +1,314 @@
-window.onload = function(){
-	var canvas = document.getElementById("canvas"),
-		context = canvas.getContext("2d"),
-		width = canvas.width = 820,
-		height = canvas.height = 560,
-		nCols = 7,
-		nRows = 5,
-		player,
-		ball,
-		bricks = [],
-		brickWidth = 56,
-		brickHeight = 28,
-		playerWidth = 120,
-		playerHeight = 5,
-		brickSep = (width - brickWidth*nCols) / (nCols+1),
-		brickSpace = (width - brickSep) / nCols,
-		mouseX, mouseY,
-		trail = [],
-		effects = [],
-		colors = ["#353941", "#ff6575", "#9be8f9"],
-		hitSound = ["audio/piano01.wav", "audio/piano02.wav"],
-		endSound = new Audio("audio/end.wav");
+window.onload = function () {
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  const width = (canvas.width = 820);
+  const height = (canvas.height = 600);
+  const nCols = 7;
+  const nRows = 5;
+  let player;
+  let ball;
+  let bricks = [];
+  const brickWidth = 56;
+  const brickHeight = 28;
+  const playerWidth = 120;
+  const playerHeight = 5;
+  const brickSep = (width - brickWidth * nCols) / (nCols + 1);
+  const brickSpace = (width - brickSep) / nCols;
+  let trail = [];
+  let effects = [];
+  const colors = ["#353941", "#ff6575", "#9be8f9"];
 
-		player = vector.create(width/2, 500);
-		ball = particle.create(width/2 - 50, 320, 5, Math.random()*Math.PI/6 - Math.PI/12 + 4*Math.PI/9);
-		ball.radius = 10;
+  player = vector.create(width / 2, 550);
+  ball = particle.create(width / 2, 520, 5, -Math.PI / 2);
+  ball.radius = 10;
 
-		var effect = {
-			x: 0,
-			y: 0,
-			t: 0,
-			create: function(x, y){
-				obj = Object.create(this);
-				obj.x = x;
-				obj.y = y;
-				return obj;
-			}
-		};
+  let effect = {
+    x: 0,
+    y: 0,
+    t: 0,
+    create: function (x, y) {
+      obj = Object.create(this);
+      obj.x = x;
+      obj.y = y;
+      return obj;
+    },
+  };
 
-		for(var i=0;i<nRows; i++){
-			for(var j=0;j<nCols; j++){
-				var brick = vector.create(brickSep + j*brickSpace + Math.random()*20-10, 45+i*40);
-				var randomColor = Math.floor(Math.random()*3 + 1);
-				brick.color = colors[randomColor - 1];
-				bricks.push(brick);
-				context.fillStyle = brick.color;
-				context.fillRect(brick.getX(), brick.getY(), brickWidth, brickHeight);
-			}
-		}
+  for (let i = 0; i < 7; i++) {
+    for (let j = 0; j < 7; j++) {
+      let brick = vector.create(205 + j * (brickWidth + 3), 120 + i * (brickHeight + 3), "#9f9da0");
+      bricks.push(brick);
+      //   context.fillStyle = brick.color;
+      //   context.fillRect(brick.getX(), brick.getY(), brickWidth, brickHeight);
+    }
+  }
 
-		mouseX = player.getX(),
-		mouseY = player.getY();
+  // Set red brick color
+  bricks[8].color = "#fa0008";
+  bricks[12].color = "#fa0008";
+  bricks[15].color = "#fa0008";
+  bricks[19].color = "#fa0008";
 
-		document.addEventListener("mousemove", function(event){
-			mouseX = event.clientX;
-			mouseY = event.clientY;
-		});
-		
-		var d, t, f, up, isShaking = false;
+  // Make yellow bricks
+  bricks.push(vector.create(264, 58, "#f0f03a"));
+  bricks.push(vector.create(264, 89, "#f0f03a"));
+  bricks.push(vector.create(204, 58, "#f0f03a"));
+  bricks.push(vector.create(204, 27, "#f0f03a"));
 
-		function shake(){
-			d = Math.random() * 3 + 1;
-			t = f = d;
-			up = false;
-			isShaking = true;
-			shakeScreen();
-		}
+  bricks.push(vector.create(500, 58, "#f0f03a"));
+  bricks.push(vector.create(500, 89, "#f0f03a"));
+  bricks.push(vector.create(559, 58, "#f0f03a"));
+  bricks.push(vector.create(559, 27, "#f0f03a"));
 
-		function shakeScreen(){
-			if(isShaking == true){
-				context.save();
-				if(up == false){
-					d -= t;		
-				}
-				else{
-					d += t;
-				}
+  // Make bottom bricks
+  bricks.push(vector.create(205, 337, "#9f9da0"));
+  bricks.push(vector.create(559, 337, "#9f9da0"));
 
-				if(d < -f){
-					up = true;
-				}
-				else if(d > f){
-					up = false;
-				}
+  bricks.push(vector.create(205, 368, "#9f9da0"));
+  bricks.push(vector.create(264, 368, "#9f9da0"));
 
-				if(t >= 0){
-					t -= 0.217;
-				}
-				else{
-					isShaking = false;
-					t = 0;
-					context.translate(0, 0);
-				}
-				context.translate(d, d);
-				requestAnimationFrame(shakeScreen);
-			}
-		}
+  bricks.push(vector.create(500, 368, "#9f9da0"));
+  bricks.push(vector.create(559, 368, "#9f9da0"));
 
-		function playSound(){
-			var n = Math.floor(Math.random()*2);
-			var audio = new Audio(hitSound[n]);
-			audio.play();
-		}
+  // Make side bricks
+  bricks.push(vector.create(146, 182, "#9f9da0"));
+  bricks.push(vector.create(146, 213, "#9f9da0"));
+  bricks.push(vector.create(146, 244, "#9f9da0"));
 
-		function blockCollision(x1, y1, x2, y2, width, height, i){
-			if(x1 + ball.radius > x2 && x1 - ball.radius < x2 + width &&
-				y1 + ball.radius > y2 && y1 - ball.radius < y2 + height){
-					if(y1 > y2 + height && x1 + ball.radius > x2 &&
-						x1 - ball.radius < x2 + width){
-						bounce("y");
-						ball.position.addTo(vector.create(0,5));
-					}
-					// up
-					else if(y1 < y2 && x1 + ball.radius > x2 &&
-						x1 - ball.radius < x2 + width){
-						bounce("y");
-						ball.position.addTo(vector.create(0,-5));
-					}
-					// left
-					else if(x1 < x2 && y1 + ball.radius > y2 &&
-						y1 - ball.radius < y2 + height){
-						bounce("x");
-						ball.position.addTo(vector.create(-5,0));
-					}
-					// right
-					else if(x1 > x2 + width && y1 + ball.radius > y2 &&
-						y1 - ball.radius < y2 + height){
-						bounce("x");
-						ball.position.addTo(vector.create(5,0));
-					}
-					playSound();
-					effects.push(effect.create(x1, y1));
-					if(isShaking == false)
-						shake();
-					return 1;	
-			}
-			else
-				return 0;		
-		}
+  bricks.push(vector.create(618, 182, "#9f9da0"));
+  bricks.push(vector.create(618, 213, "#9f9da0"));
+  bricks.push(vector.create(618, 244, "#9f9da0"));
 
-		function windowCollision(){
-			if(ball.position.getX() - ball.radius < 0 || ball.position.getX() + ball.radius > width){
-				if(ball.position.getX() - ball.radius <= 0){
-					ball.position.addTo(vector.create(2,0));
-				}
-				else{
-					ball.position.addTo(vector.create(-2,0));
-				}
-				playSound();
-				effects.push(effect.create(ball.position.getX(), ball.position.getY()));
-				if(isShaking == false)
-					shake();
-				bounce("x");
-			}
-			else if(ball.position.getY() + ball.radius > height || ball.position.getY() - ball.radius < 0){
-				if(ball.position.getY() + ball.radius >= height){
-					ball.position.addTo(vector.create(0,-2));
-				}
-				else{
-					ball.position.addTo(vector.create(0,2));
-				}
-				playSound();
-				effects.push(effect.create(ball.position.getX(), ball.position.getY()));
-				if(isShaking == false)
-					shake();
-				bounce("y");
-			}
-		}
-		function bounce(val){
-			switch(val){
-				case "x":
-					ball.velocity.setX(-ball.velocity.getX());
-					ball.velocity.setAngle(ball.velocity.getAngle() + Math.random()*0.34 - 0.17); // 0.17rad = 10deg
-					break;
-				case "y":
-					ball.velocity.setY(-ball.velocity.getY());
-					ball.velocity.setAngle(ball.velocity.getAngle() + Math.random()*0.34 - 0.17);
-					break;
-			}
-		}
+  bricks.push(vector.create(87, 213, "#9f9da0"));
+  bricks.push(vector.create(87, 244, "#9f9da0"));
+  bricks.push(vector.create(87, 275, "#9f9da0"));
+  bricks.push(vector.create(87, 306, "#9f9da0"));
+  bricks.push(vector.create(87, 337, "#9f9da0"));
 
-		function leaveTrail(){
-			trail.push([ball.position.getX(), ball.position.getY()]);
+  bricks.push(vector.create(677, 213, "#9f9da0"));
+  bricks.push(vector.create(677, 244, "#9f9da0"));
+  bricks.push(vector.create(677, 275, "#9f9da0"));
+  bricks.push(vector.create(677, 306, "#9f9da0"));
+  bricks.push(vector.create(677, 337, "#9f9da0"));
 
-			for(var i=0;i<trail.length;i++){				
-				context.beginPath();	
-				var rad = 2 + i/10;
-				context.fillStyle = "rgba(228, 47, 69, 0.05)";
-				context.arc(trail[i][0], trail[i][1], rad,
-					0, Math.PI*2, false);
-				context.fill();
-			}
-			if(trail.length > 50){
-				trail.splice(0,1);
-			}
-		}
+  let mouseX = player.getX();
 
-		function makeEffect(){
-			for(var i = 0; i < effects.length; i++){					
-				var item = effects[i];
-				context.beginPath();
-				context.arc(item.x, item.y, item.t * 15 + 10, 0, Math.PI*2, false);
-				context.fillStyle = "rgba(228, 47, 69, " + (item.t * (-0.25) + 0.25) + ")";
-				context.fill();
-				if(item.t < 1)
-					item.t += 0.05;
-				if(item.t >= 2) // make splice delay to prevent flickering
-					effects.splice(i, 1);
-			}
-		}
+  let canvasPosition = canvas.getBoundingClientRect();
 
-		function update(){
-			context.clearRect(0,0,width,height);
-			context.fillStyle = "#FFFDE0";
-			context.fillRect(0,0,width,height);
+  document.addEventListener("mousemove", function (event) {
+    mouseX = event.x - canvasPosition.left;
+  });
 
-			player.setX(Math.min(Math.max(mouseX - playerWidth/2, 0), width-playerWidth));
+  let d,
+    t,
+    f,
+    up,
+    isShaking = false;
 
-			for(var i=0; i<bricks.length; i++){
-				if(blockCollision(ball.position.getX(), ball.position.getY(),
-					bricks[i].getX(), bricks[i].getY(), brickWidth, brickHeight, i) === 1){
-					bricks.splice(i, 1);
-					if(bricks.length === 0){
-						endSound.play();
-					}
-					break;
-				}
-			}
-			blockCollision(ball.position.getX(), ball.position.getY(), player.getX(), player.getY(), playerWidth, playerHeight, 40);
-			windowCollision();
+  function shake() {
+    d = Math.random() * 3 + 1;
+    t = f = d;
+    up = false;
+    isShaking = true;
+    shakeScreen();
+  }
 
-			context.fillStyle = player.color;
-			context.fillRect(player.getX(), player.getY(), playerWidth, playerHeight);
+  function shakeScreen() {
+    if (isShaking == true) {
+      context.save();
+      if (up == false) {
+        d -= t;
+      } else {
+        d += t;
+      }
 
-			for(var i=0; i<bricks.length; i++){
-				context.fillStyle = bricks[i].color;
-				context.fillRect(bricks[i].getX(), bricks[i].getY(), brickWidth, brickHeight);
-			}
+      if (d < -f) {
+        up = true;
+      } else if (d > f) {
+        up = false;
+      }
 
-			ball.update();
-			leaveTrail();	
+      if (t >= 0) {
+        t -= 0.217;
+      } else {
+        isShaking = false;
+        t = 0;
+        context.translate(0, 0);
+      }
 
-			context.beginPath();
-			context.fillStyle = "#e42f45";
-			
-			context.arc(ball.position.getX(), ball.position.getY(), ball.radius,
-				0, Math.PI*2, false);
-			
-			context.fill();
+      context.translate(d, d);
+      requestAnimationFrame(shakeScreen);
+    }
+  }
 
-			makeEffect();
+  function blockCollision(x1, y1, x2, y2, width, height, i) {
+    if (
+      x1 + ball.radius > x2 &&
+      x1 - ball.radius < x2 + width &&
+      y1 + ball.radius > y2 &&
+      y1 - ball.radius < y2 + height
+    ) {
+      if (y1 > y2 + height && x1 + ball.radius > x2 && x1 - ball.radius < x2 + width) {
+        bounce("y");
+        ball.position.addTo(vector.create(0, 5));
+      }
+      // up
+      else if (y1 < y2 && x1 + ball.radius > x2 && x1 - ball.radius < x2 + width) {
+        bounce("y");
+        ball.position.addTo(vector.create(0, -5));
+      }
+      // left
+      else if (x1 < x2 && y1 + ball.radius > y2 && y1 - ball.radius < y2 + height) {
+        bounce("x");
+        ball.position.addTo(vector.create(-5, 0));
+      }
+      // right
+      else if (x1 > x2 + width && y1 + ball.radius > y2 && y1 - ball.radius < y2 + height) {
+        bounce("x");
+        ball.position.addTo(vector.create(5, 0));
+      }
+      effects.push(effect.create(x1, y1));
+      if (isShaking == false) shake();
+      return 1;
+    } else return 0;
+  }
 
-			if(bricks.length === 0){
-				context.fillStyle = "#000";
-				context.font = "65px Oswald";
-				context.textAlign = "center";
-				for(var i=0;i<4;i++){
-					context.fillText("You won!", width/2+i, height/2+i);
-				}
-				context.fillStyle = "#fff";
-				context.fillText("You won!", width/2, height/2);
-			}
+  function windowCollision() {
+    if (ball.position.getX() - ball.radius < 0 || ball.position.getX() + ball.radius > width) {
+      if (ball.position.getX() - ball.radius <= 0) {
+        ball.position.addTo(vector.create(2, 0));
+      } else {
+        ball.position.addTo(vector.create(-2, 0));
+      }
+      effects.push(effect.create(ball.position.getX(), ball.position.getY()));
+      if (isShaking == false) shake();
+      bounce("x");
+    } else if (ball.position.getY() + ball.radius > height || ball.position.getY() - ball.radius < 0) {
+      if (ball.position.getY() + ball.radius >= height) {
+        ball.position.addTo(vector.create(0, -2));
+      } else {
+        ball.position.addTo(vector.create(0, 2));
+      }
+      effects.push(effect.create(ball.position.getX(), ball.position.getY()));
+      if (isShaking == false) shake();
+      bounce("y");
+    }
+  }
+  function bounce(val) {
+    switch (val) {
+      case "x":
+        ball.velocity.setX(-ball.velocity.getX());
+        ball.velocity.setAngle(ball.velocity.getAngle() + Math.random() * 0.34 - 0.17); // 0.17rad = 10deg
+        break;
+      case "y":
+        ball.velocity.setY(-ball.velocity.getY());
+        ball.velocity.setAngle(ball.velocity.getAngle() + Math.random() * 0.34 - 0.17);
+        break;
+    }
+  }
 
-			context.restore();
+  function leaveTrail() {
+    trail.push([ball.position.getX(), ball.position.getY()]);
 
-			requestAnimationFrame(update);
-		}
+    for (let i = 0; i < trail.length; i++) {
+      context.beginPath();
+      let rad = 2 + i / 10;
+      context.fillStyle = "rgba(228, 47, 69, 0.05)";
+      context.arc(trail[i][0], trail[i][1], rad, 0, Math.PI * 2, false);
+      context.fill();
+    }
+    if (trail.length > 50) {
+      trail.splice(0, 1);
+    }
+  }
 
-		update();
+  function makeEffect() {
+    for (let i = 0; i < effects.length; i++) {
+      let item = effects[i];
+      context.beginPath();
+      context.arc(item.x, item.y, item.t * 15 + 10, 0, Math.PI * 2, false);
+      context.fillStyle = "rgba(228, 47, 69, " + (item.t * -0.25 + 0.25) + ")";
+      context.fill();
+      if (item.t < 1) item.t += 0.05;
+      if (item.t >= 2) {
+        // make splice delay to prevent flickering
+        effects.splice(i, 1);
+      }
+    }
+  }
+
+  function update() {
+    context.clearRect(0, 0, width, height);
+
+    player.setX(Math.min(Math.max(mouseX - playerWidth / 2, 0), width - playerWidth));
+
+    for (let i = 0; i < bricks.length; i++) {
+      if (
+        blockCollision(
+          ball.position.getX(),
+          ball.position.getY(),
+          bricks[i].getX(),
+          bricks[i].getY(),
+          brickWidth,
+          brickHeight,
+          i
+        ) === 1
+      ) {
+        bricks.splice(i, 1);
+        if (bricks.length === 0) {
+          endSound.play();
+        }
+        break;
+      }
+    }
+    blockCollision(
+      ball.position.getX(),
+      ball.position.getY(),
+      player.getX(),
+      player.getY(),
+      playerWidth,
+      playerHeight,
+      40
+    );
+    windowCollision();
+
+    context.fillStyle = player.color;
+    context.fillRect(player.getX(), player.getY(), playerWidth, playerHeight);
+
+    for (let i = 0; i < bricks.length; i++) {
+      context.fillStyle = "#000";
+      context.fillRect(bricks[i].getX() + 2, bricks[i].getY(), brickWidth, brickHeight + 2);
+      context.fillStyle = bricks[i].color;
+      context.fillRect(bricks[i].getX(), bricks[i].getY(), brickWidth, brickHeight);
+    }
+
+    ball.update();
+    leaveTrail();
+
+    context.beginPath();
+    context.fillStyle = "#e42f45";
+
+    context.arc(ball.position.getX(), ball.position.getY(), ball.radius, 0, Math.PI * 2, false);
+
+    context.fill();
+
+    makeEffect();
+
+    if (bricks.length === 0) {
+      context.fillStyle = "#000";
+      context.font = "65px Oswald";
+      context.textAlign = "center";
+      for (let i = 0; i < 4; i++) {
+        context.fillText("You won!", width / 2 + i, height / 2 + i);
+      }
+      context.fillStyle = "#fff";
+      context.fillText("You won!", width / 2, height / 2);
+    }
+
+    context.restore();
+
+    requestAnimationFrame(update);
+  }
+
+  update();
 };
-
-// Sounds:
-// https://www.freesound.org/people/keinzweiter/
-// https://www.freesound.org/people/pyzaist/sounds/118655/
-//
-//
--- lib.js
+++ lib.js
@@ -1,97 +1,100 @@
 var vector = {
-	x: 0,
-	y: 0,
-	color: "#353941",
-	create: function(x, y){
-		vec = Object.create(this);
-		vec.setX(x);
-		vec.setY(y);
-		return vec;
-	},
-	getX: function(){
-		return this.x;
-	},
-	getY: function(){
-		return this.y;
-	},
-	setX: function(x){
-		this.x = x;
-	},
-	setY: function(y){
-		this.y = y;
-	},
-	getLength: function(){
-		return Math.sqrt(this.x*this.x + this.y*this.y);
-	},
-	getAngle: function(){
-		return Math.atan2(this.y, this.x);
-	},
-	setLength: function(length){
-		var angle = this.getAngle();
-		this.x = Math.cos(angle) * length;
-		this.y = Math.sin(angle) * length;
-	},
-	setAngle: function(angle){
-		var length = this.getLength();
-		this.x = Math.cos(angle) * length;
-		this.y = Math.sin(angle) * length;
-	},
-	addTo: function(vec2){
-		 this.x += vec2.getX();
-		 this.y += vec2.getY();
-	},
-	subtractBy: function(vec2){
-		 this.x -= vec2.getX();
-		 this.y -= vec2.getY();
-	},
-	multiplyBy: function(value){
-		 this.x *= value;
-		 this.y *= value;
-	},
-	divideBy: function(value){
-		 this.x /= value;
-		 this.y /= value;
-	}
+  x: 0,
+  y: 0,
+  color: "#353941",
+  create: function (x, y, color = "#353941") {
+    vec = Object.create(this);
+    vec.setX(x);
+    vec.setY(y);
+    vec.setColor(color);
+    return vec;
+  },
+  getX: function () {
+    return this.x;
+  },
+  getY: function () {
+    return this.y;
+  },
+  setX: function (x) {
+    this.x = x;
+  },
+  setY: function (y) {
+    this.y = y;
+  },
+  setColor: function (color) {
+    this.color = color;
+  },
+  getLength: function () {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  },
+  getAngle: function () {
+    return Math.atan2(this.y, this.x);
+  },
+  setLength: function (length) {
+    var angle = this.getAngle();
+    this.x = Math.cos(angle) * length;
+    this.y = Math.sin(angle) * length;
+  },
+  setAngle: function (angle) {
+    var length = this.getLength();
+    this.x = Math.cos(angle) * length;
+    this.y = Math.sin(angle) * length;
+  },
+  addTo: function (vec2) {
+    this.x += vec2.getX();
+    this.y += vec2.getY();
+  },
+  subtractBy: function (vec2) {
+    this.x -= vec2.getX();
+    this.y -= vec2.getY();
+  },
+  multiplyBy: function (value) {
+    this.x *= value;
+    this.y *= value;
+  },
+  divideBy: function (value) {
+    this.x /= value;
+    this.y /= value;
+  },
 };
 
 var particle = {
-	position:null,
-	velocity:null,
-	mass:null,
-	gravity:null,
-	radius:0,
-	create: function(x, y, speed, direction, grav){
-		p = Object.create(this);
-		p.position = vector.create(x,y);
-		p.velocity = vector.create(0,0);
-		p.velocity.setLength(speed);
-		p.velocity.setAngle(direction);
-		p.gravity = vector.create(0, grav || 0);
-		return p;
-	},
-	accelerate: function(accel){
-		this.velocity.addTo(accel);
-	},
-	update: function(){
-		this.position.addTo(this.velocity);
-		this.velocity.addTo(this.gravity);
-	},
-	angleTo: function(p2){
-		return Math.atan2(p2.position.getY() - this.position.getY(),
-							p2.position.getX() - this.position.getX());
-	},
-	distanceTo: function(p2){
-		var pX = this.position.getX() - p2.position.getX(),
-			pY = this.position.getY() - p2.position.getY();
-		return Math.sqrt( pX*pX + pY*pY);
-	},
-	gravitateTo: function(p2){
-		var grav = vector.create(0,0),
-			dist = this.distanceTo(p2);
+  position: null,
+  velocity: null,
+  mass: null,
+  gravity: null,
+  radius: 0,
+  create: function (x, y, speed, direction, grav) {
+    p = Object.create(this);
+    p.position = vector.create(x, y);
+    p.velocity = vector.create(0, 0);
+    p.velocity.setLength(speed);
+    p.velocity.setAngle(direction);
+    p.gravity = vector.create(0, grav || 0);
+    return p;
+  },
+  accelerate: function (accel) {
+    this.velocity.addTo(accel);
+  },
+  update: function () {
+    this.position.addTo(this.velocity);
+    this.velocity.addTo(this.gravity);
+  },
+  angleTo: function (p2) {
+    return Math.atan2(p2.position.getY() - this.position.getY(), p2.position.getX() - this.position.getX());
+  },
+  distanceTo: function (p2) {
+    var pX = this.position.getX() - p2.position.getX(),
+      pY = this.position.getY() - p2.position.getY();
+    return Math.sqrt(pX * pX + pY * pY);
+  },
+  gravitateTo: function (p2) {
+    var grav = vector.create(0, 0),
+      dist = this.distanceTo(p2);
 
-		grav.setLength(p2.mass / (dist*dist));
-		grav.setAngle(this.angleTo(p2));
+    grav.setLength(p2.mass / (dist * dist));
+    grav.setAngle(this.angleTo(p2));
 
-		this.velocity.addTo(grav);
-	}
+    this.velocity.addTo(grav);
+  },
 };
